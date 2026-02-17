999b36315751598518131aa0f7ab3d9b5dba9fec
Odo599 <owen00064@gmail.com>
2026-02-18 08:24:15 +1100

    added navigation and status bar colour

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,20 +1,22 @@
-import {ScrollView, Text, View} from "react-native";
+import {ScrollView, View} from "react-native";
 import TopHeader from "@/app/components/TopHeader";
+import EntriesView from "@/app/components/EntriesView";
 
 
 export default function Index() {
     // @ts-ignore
     return (
-        <View
-            style={{
-                flex: 1,
-                padding: 20
-            }}
-        >
-            <ScrollView>
-                <TopHeader/>
-                <Text>More</Text>
-            </ScrollView>
-        </View>
+        <>
+            <View
+                style={{
+                    flex: 1
+                }}
+            >
+                <ScrollView>
+                    <TopHeader/>
+                    <EntriesView/>
+                </ScrollView>
+            </View>
+        </>
     );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,24 @@
 // noinspection JSUnusedGlobalSymbols
 
 import {Stack} from "expo-router";
+import styles from "@/app/styles"
+import {SafeAreaView} from "react-native-safe-area-context";
+import {StatusBar} from "react-native";
 
 export default function RootLayout() {
     return (
-        <Stack>
-            <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
-        </Stack>
+        <>
+            <StatusBar
+                barStyle="dark-content"
+                backgroundColor={styles.topHeader.backgroundColor}
+                translucent={false}
+            />
+            <SafeAreaView style={{flex: 1, backgroundColor: styles.topHeader.backgroundColor}}>
+                <Stack>
+                    <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
+                </Stack>
+            </SafeAreaView>
+        </>
+
     );
 }
