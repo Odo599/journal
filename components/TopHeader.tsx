1e46eb50f6c0fdbbc5c5314b74b972da3ef54211
Odo599 <owen00064@gmail.com>
2026-02-21 07:45:20 +1100

    modified TopHeader.tsx to a nicer design

--- components/TopHeader.tsx
+++ components/TopHeader.tsx
@@ -1,6 +1,5 @@
 import {View, Text} from "react-native";
 import TopHeaderStyles from "@/styles/TopHeaderStyles"
-import Icon from 'react-native-vector-icons/MaterialIcons';
 import {useFonts, SourceSansPro_700Bold} from '@expo-google-fonts/source-sans-pro';
 
 export default function TopHeader() {
@@ -11,7 +10,6 @@ export default function TopHeader() {
         return <Text>Loading...</Text>
     }
     return <View style={TopHeaderStyles.topHeader}>
-        <Icon name="menu"/>
         <Text style={TopHeaderStyles.topHeaderTitle}>Journal</Text>
     </View>
 }
--- styles/TopHeaderStyles.ts
+++ styles/TopHeaderStyles.ts
@@ -2,11 +2,12 @@ import {StyleSheet} from "react-native";
 
 const TopHeaderStyles = StyleSheet.create({
     topHeader: {
-        height: 150,
-        backgroundColor: "#999999",
-        borderBottomLeftRadius: 20,
-        borderBottomRightRadius: 20,
+        height: 100,
         padding: 17.5,
+        borderColor: "grey",
+        borderWidth: 2,
+        margin: 10,
+        borderRadius: 20
     },
     topHeaderTitle: {
         fontSize: 50,
