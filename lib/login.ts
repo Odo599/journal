5938ece3f65068d91609040da43e0ac7c37d1a59
Odo599 <owen00064@gmail.com>
2026-02-20 17:43:44 +1100

    removed catching from login func

--- lib/login.ts
+++ lib/login.ts
@@ -1,14 +1,8 @@
 export default async function login(username: string, password: string) {
-    try {
-        const url = `http://127.0.0.1:8000/getUserApiKey?username=${username}&password=${password}`
-        const response = await fetch(url, {method: "GET"})
-        if (!response.ok) {
-            console.error("Response status", response.status);
-        }
-        const result = await response.text()
-        console.log("api key", result)
-    } catch (error) {
-        // @ts-ignore
-        console.error(error)
+    const url = `http://127.0.0.1:8000/getUserApiKey?username=${username}&password=${password}`
+    const response = await fetch(url, {method: "GET"})
+    if (!response.ok) {
+        console.error("Response status", response.status);
     }
+    return await response.text()
 }
